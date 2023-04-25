172b567f8db61e636565ff3aeaff70c824300038
dacasfilipe <filiperdacas@gmail.com>
2023-04-24 21:28:55 -0300

    inserido os arquivos iniciais da aplicação

---
+++ appNode.js
@@ -0,0 +1,16 @@
+//cria uma constante que recebe a biblioteca http
+const http = require('http');
+//cria uma constante que recebe o endereço 
+//local da minha máquina e a porta
+const hostname = '127.0.0.1'; //localhost
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello world');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
